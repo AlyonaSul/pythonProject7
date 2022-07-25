71ee7d43d2a53e28f01b95b05459e1cac4209a17
Alyona S <alyonasul6277@gmail.com>
2022-07-25 23:30:39 +0300

    Zadanie12.7.3

--- HomeWork.py
+++ HomeWork.py
@@ -3,6 +3,7 @@ per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 deposit = []
 
 for i in per_cent.values():
-    deposit.append(i* money/100)
+    deposit.append(int(i* money/100))
 
 print(deposit)
+print (max(deposit))
